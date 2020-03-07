c65dc445d57207bffea84a49e5079d8e86c7ca09
Priyaraj7 <suppichala@gmail.com>
2020-03-07 14:03:47 -0800

    complete backend and start work on UI

--- src/age.js
+++ src/age.js
@@ -1,5 +1,3 @@
-
-// calculate age
 export class PersonAge {
   constructor(dob) {
     // [1] new Date(dateString)
@@ -25,56 +23,69 @@ export class PersonAge {
     return `Your age on Earth is ${this.calculateAge()} years`;
   }
   calculateAgeOnMercury() {
-    return `Your age on Mercury is ${Math.ceil(this.calculateAge() * 365 / 88)} years`
+    return `Your age on Mercury is ${Math.ceil(
+      (this.calculateAge() * 365) / 88
+    )} years`;
   }
   calculateAgeOnVenus() {
-    return `Your age on Venus is ${Math.ceil(this.calculateAge() * 365 / 225)} years`
+    return `Your age on Venus is ${Math.ceil(
+      (this.calculateAge() * 365) / 225
+    )} years`;
   }
   calculateAgeOnMars() {
-    return `Your age on Mars is ${Math.ceil(this.calculateAge() * 365 / 687)} years`
+    return `Your age on Mars is ${Math.ceil(
+      (this.calculateAge() * 365) / 687
+    )} years`;
   }
   calculateAgeOnJupiter() {
-    return `Your age on Jupiter is ${Math.ceil(this.calculateAge()/ 12)} years`
+    return `Your age on Jupiter is ${Math.ceil(
+      this.calculateAge() / 12
+    )} years`;
   }
 
   getStatusOnEarth() {
-        const age = this.calculateAge();
-        const lifeExpectancy = 79;
-         if(lifeExpectancy >= age) {
-           return `You have ${lifeExpectancy - age} years left on Earth`;
-         } else {
-           return `You have lived past ${age - lifeExpectancy } years on Earth`;
-         }
-      }
-      getStatusOnMercury() {
-        const age = Math.ceil(this.calculateAge() * 365 / 88);
-        const lifeExpectancy = Math.ceil(79 * 365 / 88)
-         if(lifeExpectancy >= age) {
-           return `You have ${lifeExpectancy - age} years left on Mercury`;
-         } else {
-           return `You have lived past ${age - lifeExpectancy } years on Mercury`;
-         }
-      }
-      getStatusOnVenus() {
-        const age = Math.ceil(this.calculateAge() * 365 / 225);
-        const lifeExpectancy = Math.ceil(79 * 365 / 225)
-         if(lifeExpectancy >= age) {
-           return `You have ${lifeExpectancy - age} years left on Venus`;
-         } else {
-           return `You have lived past ${age - lifeExpectancy } years on Venus`;
-         }
-      }
-      getStatusOnMars() {
-        const age = Math.ceil(this.calculateAge() * 365 / 687);
-        const lifeExpectancy = Math.ceil(79 * 365 / 687)
-         if(lifeExpectancy >= age) {
-           return `You have ${lifeExpectancy - age} years left on Mars`;
-         } else {
-           return `You have lived past ${age - lifeExpectancy } years on Mars`;
-         }
-      }
-
-
+    const age = this.calculateAge();
+    const lifeExpectancy = 79;
+    if (lifeExpectancy >= age) {
+      return `You have ${lifeExpectancy - age} years left on Earth`;
+    } else {
+      return `You have lived past ${age - lifeExpectancy} years on Earth`;
+    }
+  }
+  getStatusOnMercury() {
+    const age = Math.ceil((this.calculateAge() * 365) / 88);
+    const lifeExpectancy = Math.ceil((79 * 365) / 88);
+    if (lifeExpectancy >= age) {
+      return `You have ${lifeExpectancy - age} years left on Mercury`;
+    } else {
+      return `You have lived past ${age - lifeExpectancy} years on Mercury`;
+    }
+  }
+  getStatusOnVenus() {
+    const age = Math.ceil((this.calculateAge() * 365) / 225);
+    const lifeExpectancy = Math.ceil((79 * 365) / 225);
+    if (lifeExpectancy >= age) {
+      return `You have ${lifeExpectancy - age} years left on Venus`;
+    } else {
+      return `You have lived past ${age - lifeExpectancy} years on Venus`;
+    }
+  }
+  getStatusOnMars() {
+    const age = Math.ceil((this.calculateAge() * 365) / 687);
+    const lifeExpectancy = Math.ceil((79 * 365) / 687);
+    if (lifeExpectancy >= age) {
+      return `You have ${lifeExpectancy - age} years left on Mars`;
+    } else {
+      return `You have lived past ${age - lifeExpectancy} years on Mars`;
+    }
+  }
+  getStatusOnJupiter() {
+    const age = Math.ceil(this.calculateAge() / 12);
+    const lifeExpectancy = Math.ceil(79 / 12);
+    if (lifeExpectancy >= age) {
+      return `You have ${lifeExpectancy - age} years left on Jupiter`;
+    } else {
+      return `You have lived past ${age - lifeExpectancy} years on Jupiter`;
+    }
+  }
 }
-
-
